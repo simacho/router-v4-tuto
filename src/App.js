5601f9665dd56f44c55ed0ac512e522b1061fd1d
simazaki katsuya <simazakikatuya@gmail.com>
2019-01-29 02:39:26 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,28 +1,104 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
-class App extends Component {
-  render() {
+const App = () => (
+    <BrowserRouter>
+    <div>
+    <ul>
+    <li><Link to='/'>Home</Link></li>
+    <li><Link to='/about'>About</Link></li>
+    <li><Link to='/friends'>Friends</Link></li>
+    </ul>
+
+    <hr />
+
+    <Route exact path='/' component={Home} />
+    <Route path='/about' component={About} />
+    <Route path='/friends' component={Friends} />
+    </div>
+    </BrowserRouter>
+)
+
+const Home = () => (
+    <div>
+    <h2>Home</h2>
+    <p>Welcome to ようこそ</p>
+    </div>
+)
+const About = () => (
+    <div>
+    <h2>About</h2>
+    <p>フレンズに投票するページです</p>
+    </div>
+)
+const Friends = () => (
+    <div>
+    <h2>Friends</h2>
+    <Route exact path='/friends' component={FriendList} />
+    <Route path='/friends/:id' component={Friend} />
+    </div>
+)
+
+const FRIENDS = [
+    {
+        id: 'serval',
+        nameJa: 'サーバル',
+        nameEn: 'Serval Cat',
+        family: 'ネコ目ネコ科ネコ属'
+    },
+    {
+        id: 'raccoon',
+        nameJa: 'アライグマ',
+        nameEn: 'Common raccoon',
+        family: 'ネコ目アライグマ科アライグマ属'
+    },
+    {
+        id: 'fennec',
+        nameJa: 'フェネック',
+        nameEn: 'Fennec',
+        family: 'ネコ目イヌ科キツネ属'
+    }
+]
+
+const friendById = id => FRIENDS.find(friend => friend.id === id)
+
+
+const FriendList = () => (
+    <div>
+    {FRIENDS.map(friend => (
+        <li key={friend.id}>
+        <Link to={`/friends/${friend.id}`}>{friend.nameJa}</Link>
+        </li>
+    ))}
+    </div>
+)
+
+const Friend = props => {
+    const { id } = props.match.params
+    const friend = friendById(id)
+
+    if (typeof friend === 'undefined')  {
+        return (
+            <div>
+            <p>Friends with id '{id}' does not exist.</p>
+            </div>
+        )
+    }
+
+
+    const containerStyle = { border: '1px gray solid', display: 'inline-block', padding: 10 }
+    const contentsStyle = { margin: 0 }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+        <div>
+        <div style={containerStyle}>
+        <p style={contentsStyle}>{friend.family}</p>
+        <h1 style={contentsStyle}>{friend.nameJa}</h1>
+        <p style={contentsStyle}>{friend.nameEn}</p>
+        </div>
+        </div>
+    )
 }
 
-export default App;
+
+export default App
